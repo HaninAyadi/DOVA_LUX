a8334f43c3af1f525ea7e5f6afa7b17210026544
Hanin Ayadi <Hanin.Ayadi@lih.lu>
2023-02-09 13:05:44 +0100

    more comments

--- utils/clustering/clustering_viz.py
+++ utils/clustering/clustering_viz.py
@@ -6,12 +6,20 @@ from wordcloud import WordCloud
 from yellowbrick.text import TSNEVisualizer
 import matplotlib.pyplot as plt
 
+# Setting general figure parameters for plt
 plt.rcParams["figure.figsize"] = (15, 10)
 plt.rcParams["legend.fontsize"] = 15
 plt.rcParams["axes.titlesize"] = 15
 
 
 def create_wordcloud(data, title=None):
+    """Generates wordcloud out of text data.
+
+    Args:
+        data: Text data.
+        title: Title of the wordcloud.
+
+    """
     wordcloud = WordCloud(width=500, height=500,
                           background_color='white',
                           min_font_size=15).generate(" ".join(data))
@@ -25,6 +33,15 @@ def create_wordcloud(data, title=None):
 
 
 def tsne_viz(X, labels, colors=None, colormap='gist_ncar'):
+    """Generates t-sne visualization for classified data.
+
+    Args:
+        X: Matrix of features (text embeddings or vectors).
+        labels: Assigned cluster / class for each input.
+        colors: Custom list of the colors to use for the plot.
+        colormap: Matplotlib colormap used when colors are not defined.
+
+    """
     if colors == None:
         tsne = TSNEVisualizer(colormap=colormap)
     else:
